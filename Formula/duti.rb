f6369d1443608e9b3c7cb7b2b5e565c715c0351d
yemreak <yemreak@icloud.com>
2026-05-14 18:03:19 +0300

    duti duti-v0.1.0

--- Formula/duti.rb
+++ Formula/duti.rb
@@ -2,7 +2,7 @@ class Duti < Formula
   desc     "Open code and text files in Cursor"
   homepage "https://github.com/yemreak/homebrew-tap"
   url      "https://github.com/yemreak/homebrew-tap/releases/download/duti-v0.1.0/duti-cli-arm64.zip"
-  sha256   "339019270f8a3d57d0a2c6e69010acd66ee8d181c79b3e3904fc3853b797b1bf"
+  sha256   "9c03ebf81ab5109780f55bc248fdc13b380f721f76ba829186a7065f672a8225"
   version  "0.1.0"
   license  :cannot_represent
 
