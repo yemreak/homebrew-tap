c018557c72a7493fea32e28c6d2c8a05145e1e21
yemreak <yemreak@icloud.com>
2026-05-15 12:00:58 +0300

    openwith-cursor openwith-cursor-v0.1.2

--- Formula/openwith-cursor.rb
+++ Formula/openwith-cursor.rb
@@ -1,9 +1,9 @@
 class OpenwithCursor < Formula
   desc     "Open code and text files in Cursor"
   homepage "https://github.com/yemreak/homebrew-tap"
-  url      "https://github.com/yemreak/homebrew-tap/releases/download/openwith-cursor-v0.1.1/openwith-cursor-arm64.zip"
-  sha256   "f1cb2aa8f3fe29632ca63ef393e7a00b7dbaa6adc049ca2344aff7749821cc4e"
-  version  "0.1.1"
+  url      "https://github.com/yemreak/homebrew-tap/releases/download/openwith-cursor-v0.1.2/openwith-cursor-arm64.zip"
+  sha256   "8ac3358be87a5fa897b03ddd40075e701163f244eee204f1fbea244d23a8be85"
+  version  "0.1.2"
   license  :cannot_represent
 
   def install
